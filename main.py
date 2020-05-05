ea7311a1f7aea58f4022222daddc6a4a90b4e411
Zalak <zalakpatel534@gmail.com>
2020-05-05 13:56:27 +0530

    initialize

---
+++ main.py
@@ -0,0 +1,8 @@
+# deviation
+# n = int(input("Enter total elements in array (5 to 100)"))
+# arr = list(map(int, input("Enter elements in array (0 to 100000)").split()))
+
+n = 5
+arr = [10, 40, 30, 50, 20]
+
+deviation =
