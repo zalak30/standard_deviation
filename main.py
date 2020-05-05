7ce5b0fc5a6e212564d1cfb44a616341c4078ddd
Zalak <zalakpatel534@gmail.com>
2020-05-05 15:31:42 +0530

    find S.D

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # deviation
-# n = int(input("Enter total elements in array (5 to 100)"))
-# arr = list(map(int, input("Enter elements in array (0 to 100000)").split()))
 
-n = 5
-arr = [10, 40, 30, 50, 20]
+from statistics import mean
+import math
+n = int(input("Enter total elements in array (5 to 100)"))
+arr = list(map(int, input("Enter elements in array (0 to 100000)").split()))
 
-deviation =
+xi = []
+for num in arr:
+    xi.append((num-mean(arr))**2)
+print(round(math.sqrt(sum(xi)/n), 1))
